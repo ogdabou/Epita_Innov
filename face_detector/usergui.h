4a0034ca15b9a2434c9edc90e1838ee075770830
Marion <piednoir-N55SF>
2014-02-03 00:28:20 +0100

    server msg

--- face_detector/usergui.h
+++ face_detector/usergui.h
@@ -2,12 +2,13 @@
 #define USERGUI_H
 #include "mainHeader.hpp"
 #include "interestzone.h"
+#include "Websocketserver.h"
 
 class UserGUI
 {
 public:
     UserGUI();
-    cv::Mat print(cv::Mat frame, std::vector<InterestZone> &zone_interest);
+    cv::Mat print(cv::Mat frame, std::vector<InterestZone> &zone_interest, WebSocketServer* myServer);
 };
 
 #endif // USERGUI_H
